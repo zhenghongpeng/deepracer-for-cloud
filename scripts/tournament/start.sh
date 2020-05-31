2afc7130130388830e34b6d2d748374d98b36c6c
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-05-31 18:32:30 +0000

    Adding other start script

---
+++ scripts/tournament/start.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+source $DR_DIR/bin/scripts_wrapper.sh
+
+usage(){
+	echo "Usage: $0 [-f yaml-file]"
+  echo "       -f filename  Tournament Yaml configuration."
+	exit 1
+}
+
+trap ctrl_c INT
+
+function ctrl_c() {
+        echo "Requested to stop."
+        exit 1
+}
+
+while getopts ":f:" opt; do
+case $opt in
+f) OPT_YAML_FILE="$OPTARG"
+;;
+h) usage
+;;
+\?) echo "Invalid option -$OPTARG" >&2
+usage
+;;
+esac
+done
+
+# set evaluation specific environment variables
+S3_PATH="s3://$DR_LOCAL_S3_BUCKET/$DR_LOCAL_S3_MODEL_PREFIX"
+STACK_NAME="deepracer-eval-$DR_RUN_ID"
+
+export ROBOMAKER_COMMAND="bash"
+export DR_CURRENT_PARAMS_FILE=${DR_LOCAL_S3_EVAL_PARAMS_FILE}
+
+if [ ${DR_ROBOMAKER_MOUNT_LOGS,,} = "true" ];
+then
+  COMPOSE_FILES="$DR_EVAL_COMPOSE_FILE -c $DR_DIR/docker/docker-compose-mount.yml"
+  export DR_MOUNT_DIR="$DR_DIR/data/logs/robomaker/$DR_LOCAL_S3_MODEL_PREFIX"
+  mkdir -p $DR_MOUNT_DIR
+else
+  COMPOSE_FILES="$DR_EVAL_COMPOSE_FILE"
+fi
+
+echo "Upload configuration to $S3_PATH/$DR_CURRENT_PARAMS_FILE"
+aws ${DR_LOCAL_PROFILE_ENDPOINT_URL} s3 cp $OPT_YAML_FILE $S3_PATH/$DR_CURRENT_PARAMS_FILE
+#python3 prepare-config.py
+
+docker stack deploy $COMPOSE_FILES $STACK_NAME
+
+echo 'waiting for containers to start up...'
+
+#sleep for 20 seconds to allow the containers to start
+sleep 15
+
+if xhost >& /dev/null;
+then
+  echo "Display exists, using gnome-terminal for logs and starting vncviewer."
+
+  echo 'attempting to pull up sagemaker logs...'
+  gnome-terminal -x sh -c "!!; docker logs -f $(docker ps | awk ' /robomaker/ { print $1 }')"
+
+  echo 'attempting to open vnc viewer...'
+  gnome-terminal -x sh -c "!!; vncviewer localhost:8080"
+else
+  echo "No display. Falling back to CLI mode."
+  docker logs -f $(docker ps | awk ' /robomaker/ { print $1 }')
+fi
