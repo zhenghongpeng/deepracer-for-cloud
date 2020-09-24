ed218cac5e070eea020ff35a6f4fe08e789c6de1
zhenghongpeng <zhenghongpengcs@gmail.com>
2020-09-24 09:26:43 -0500

    log analysis

--- scripts/training/back-up-training-run.sh
+++ scripts/training/back-up-training-run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BACKUP_LOC=/media/aschu/storage/deepracer-training/backup
+BACKUP_LOC=/tmp/deepracer-training/backup
 FILENAME=$(date +%Y-%m-%d_%H-%M-%S)
-tar -czvf ${FILENAME}.tar.gz ../../docker/volumes/minio/bucket/rl-deepracer-sagemaker/*
+tar -czvf ${FILENAME}.tar.gz /home/zpeng/code/deepracer-for-dummies/docker/volumes/minio/bucket/rl-deepracer-sagemaker/*
 mv ${FILENAME}.tar.gz $BACKUP_LOC
